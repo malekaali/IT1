577a6e5be9cd87ba3a192170fffc8a3873a1288c
User <User@172.31.42.36>
2016-03-24 11:49:49 +0200

    exception

--- src/aaaaaaa/asd.java
+++ src/aaaaaaa/asd.java
@@ -3,7 +3,9 @@ public class asd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+welcom();
+int x=0;
+x=x/x;
 	}
 public static void welcom(){
 	system.out.println("hello world");
