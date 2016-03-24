c9bcbaf436f0851c2439ac7fb897e838ab76fde7
User <User@172.31.42.36>
2016-03-24 11:55:05 +0200

    Revert "exception"
    This reverts commit 577a6e5be9cd87ba3a192170fffc8a3873a1288c.

--- src/aaaaaaa/asd.java
+++ src/aaaaaaa/asd.java
@@ -3,9 +3,7 @@ public class asd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-welcom();
-int x=0;
-x=x/x;
+
 	}
 public static void welcom(){
 	system.out.println("hello world");
